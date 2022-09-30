0aae5a7bba2cc04e4e1f67e7909c8800424e908c
Mayur <guravmayur7@gmail.com>
2022-09-30 20:47:32 +0530

    signup api integration

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const cors = require("cors");
+require("./DB/config");
+const User = require("./DB/User");
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-const connectDB = async () => {
-  mongoose.connect("mongodb://localhost:27017/e-com");
-  const productSchema = new mongoose.Schema({});
-  const product = mongoose.model("products", productSchema);
-  const data = await product.find();
-  console.warn(data);
-};
-connectDB();
+app.post("/register", async (req, resp) => {
+  const userData = new User(req.body);
+  const result = await userData.save();
+  resp.send(result);
+});
+
+// const connectDB = async () => {
+//   mongoose.connect("mongodb://0.0.0.0:27017/e-commerce");
+//   const productSchema = new mongoose.Schema({});
+//   const product = mongoose.model("products", productSchema);
+//   const data = await product.find();
+//   console.warn(data);
+// };
+// connectDB();
 // app.get("/", (req, resp) => {
 //   resp.send("App is setup in backend");
 // });
